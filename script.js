011a7f4d88333467483a9ed954f331a191b39374
domesatyre <147668508+domesatyre@users.noreply.github.com>
2024-06-16 22:31:54 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,57 @@
+console.log('Script chargé');
+
+const frameContainer = document.querySelector('.frame-container');
+let frameIndex = 1;
+let prevFrameIndex = 156;
+let nextFrameIndex = 2;
+
+// Chemin vers le dossier des images
+const imagePath = 'image/'; // attention au slash à la fin
+
+// Précharger les images précédentes et suivantes
+const prevFrameElement = new Image();
+const nextFrameElement = new Image();
+prevFrameElement.src = `${imagePath}ezgif-frame-${prevFrameIndex.toString().padStart(10, '0')}.jpg`;
+nextFrameElement.src = `${imagePath}ezgif-frame-${nextFrameIndex.toString().padStart(10, '0')}.jpg`;
+
+frameContainer.addEventListener('wheel', (event) => {
+  event.preventDefault(); // empêcher le défilement par défaut
+
+  // Déterminer la direction du défilement
+  const delta = Math.sign(event.deltaY);
+
+  // Mettre à jour l'index de l'image en fonction de la direction du défilement
+  frameIndex += delta;
+  if (frameIndex < 1) {
+    frameIndex = 156;
+  } else if (frameIndex > 156) {
+    frameIndex = 1;
+  }
+
+  // Mettre à jour les index des images précédentes et suivantes
+  if (delta < 0) {
+    prevFrameIndex = frameIndex;
+    nextFrameIndex = frameIndex + 1;
+    if (nextFrameIndex > 156) {
+      nextFrameIndex = 1;
+    }
+  } else {
+    nextFrameIndex = frameIndex;
+    prevFrameIndex = frameIndex - 1;
+    if (prevFrameIndex < 1) {
+      prevFrameIndex = 156;
+    }
+  }
+
+  // Précharger les images précédentes et suivantes
+  prevFrameElement.src = `${imagePath}ezgif-frame-${prevFrameIndex.toString().padStart(3, '0')}.jpg`;
+  nextFrameElement.src = `${imagePath}ezgif-frame-${nextFrameIndex.toString().padStart(3, '0')}.jpg`;
+
+  // Mettre à jour l'image affichée
+  const frameElement = document.querySelector('.frame');
+  frameElement.src = `${imagePath}ezgif-frame-${frameIndex.toString().padStart(3, '0')}.jpg`;
+
+  console.log(`frameIndex: ${frameIndex}`);
+  console.log(`prevFrameIndex: ${prevFrameIndex}`);
+  console.log(`nextFrameIndex: ${nextFrameIndex}`);
+});
