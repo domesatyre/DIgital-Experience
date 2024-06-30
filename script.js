e34b8040b81f601f38322c294c7fc4fed5ffacad
domesatyre <147668508+domesatyre@users.noreply.github.com>
2024-06-30 18:46:16 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -2,17 +2,17 @@ console.log('Script chargé');
 
 const frameContainer = document.querySelector('.frame-container');
 let frameIndex = 1;
-let prevFrameIndex = 156;
+let prevFrameIndex = 299;
 let nextFrameIndex = 2;
 
-// Chemin vers les images
-const imagePath = ''; // plus besoin du dossier image/
+// Chemin vers le dossier des images
+const imagePath = 'image/ezgif-frame_'; // attention au slash à la fin
 
 // Précharger les images précédentes et suivantes
 const prevFrameElement = new Image();
 const nextFrameElement = new Image();
-prevFrameElement.src = `ezgif-frame-${prevFrameIndex.toString().padStart(10, '0')}.jpg`;
-nextFrameElement.src = `ezgif-frame-${nextFrameIndex.toString().padStart(10, '0')}.jpg`;
+prevFrameElement.src = `${imagePath}${prevFrameIndex.toString().padStart(3, '0')}.jpg`;
+nextFrameElement.src = `${imagePath}${nextFrameIndex.toString().padStart(3, '0')}.jpg`;
 
 frameContainer.addEventListener('wheel', (event) => {
   event.preventDefault(); // empêcher le défilement par défaut
@@ -23,8 +23,8 @@ frameContainer.addEventListener('wheel', (event) => {
   // Mettre à jour l'index de l'image en fonction de la direction du défilement
   frameIndex += delta;
   if (frameIndex < 1) {
-    frameIndex = 156;
-  } else if (frameIndex > 156) {
+    frameIndex = 299;
+  } else if (frameIndex > 299) {
     frameIndex = 1;
   }
 
@@ -32,24 +32,24 @@ frameContainer.addEventListener('wheel', (event) => {
   if (delta < 0) {
     prevFrameIndex = frameIndex;
     nextFrameIndex = frameIndex + 1;
-    if (nextFrameIndex > 156) {
+    if (nextFrameIndex > 299) {
       nextFrameIndex = 1;
     }
   } else {
     nextFrameIndex = frameIndex;
     prevFrameIndex = frameIndex - 1;
     if (prevFrameIndex < 1) {
-      prevFrameIndex = 156;
+      prevFrameIndex = 299;
     }
   }
 
   // Précharger les images précédentes et suivantes
-  prevFrameElement.src = `ezgif-frame-${prevFrameIndex.toString().padStart(3, '0')}.jpg`;
-  nextFrameElement.src = `ezgif-frame-${nextFrameIndex.toString().padStart(3, '0')}.jpg`;
+  prevFrameElement.src = `${imagePath}${prevFrameIndex.toString().padStart(3, '0')}.jpg`;
+  nextFrameElement.src = `${imagePath}${nextFrameIndex.toString().padStart(3, '0')}.jpg`;
 
   // Mettre à jour l'image affichée
   const frameElement = document.querySelector('.frame');
-  frameElement.src = `ezgif-frame-${frameIndex.toString().padStart(3, '0')}.jpg`;
+  frameElement.src = `${imagePath}${frameIndex.toString().padStart(3, '0')}.jpg`;
 
   console.log(`frameIndex: ${frameIndex}`);
   console.log(`prevFrameIndex: ${prevFrameIndex}`);
